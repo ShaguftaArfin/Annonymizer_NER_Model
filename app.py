5570e1f55962caf58ffc73073be8b1b06e4011e7
Shagufta Arfin <77445239+ShaguftaArfin@users.noreply.github.com>
2023-02-02 01:19:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import spacy
 from annotated_text import annotated_text
 import requests
 import streamlit as st
-from streamlit_lottie import st_lottie
+#from streamlit_lottie import st_lottie
 from PIL import Image
 from streamlit_option_menu import option_menu
 
