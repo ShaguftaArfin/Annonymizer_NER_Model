81f6194ac18ab1196fa3ede1e9a5ca38bbc2e3f1
Shagufta Arfin <77445239+ShaguftaArfin@users.noreply.github.com>
2023-02-02 01:19:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import requests
 import streamlit as st
 #from streamlit_lottie import st_lottie
 from PIL import Image
-from streamlit_option_menu import option_menu
+#from streamlit_option_menu import option_menu
 
 # Find more emojis here: https://www.webfx.com/tools/emoji-cheat-sheet/
 st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
